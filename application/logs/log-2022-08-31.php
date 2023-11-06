3512dd191123cbcbd969386230b9b2c9fad92ddd
TheFudx <70996626+TheFudx@users.noreply.github.com>
2023-11-06 23:22:37 +0530

    Add files via upload

---
+++ application/logs/log-2022-08-31.php
@@ -0,0 +1,267 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2022-08-31 06:18:04 --> Config Class Initialized
+INFO - 2022-08-31 06:18:04 --> Hooks Class Initialized
+DEBUG - 2022-08-31 06:18:04 --> UTF-8 Support Enabled
+INFO - 2022-08-31 06:18:04 --> Utf8 Class Initialized
+INFO - 2022-08-31 06:18:04 --> URI Class Initialized
+INFO - 2022-08-31 06:18:04 --> Router Class Initialized
+INFO - 2022-08-31 06:18:04 --> Output Class Initialized
+INFO - 2022-08-31 06:18:05 --> Security Class Initialized
+DEBUG - 2022-08-31 06:18:05 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 06:18:05 --> Input Class Initialized
+INFO - 2022-08-31 06:18:05 --> Language Class Initialized
+INFO - 2022-08-31 06:18:05 --> Loader Class Initialized
+INFO - 2022-08-31 06:18:05 --> Helper loaded: form_helper
+INFO - 2022-08-31 06:18:05 --> Helper loaded: url_helper
+INFO - 2022-08-31 06:18:05 --> Helper loaded: file_helper
+INFO - 2022-08-31 06:18:05 --> Helper loaded: data_helper
+INFO - 2022-08-31 06:18:05 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 06:18:05 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 06:18:05 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 06:18:05 --> Form Validation Class Initialized
+INFO - 2022-08-31 06:18:05 --> Controller Class Initialized
+INFO - 2022-08-31 06:18:05 --> MY_Model class loaded
+INFO - 2022-08-31 06:18:05 --> Model "Usermodel" initialized
+INFO - 2022-08-31 06:18:05 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/login.php
+INFO - 2022-08-31 06:18:05 --> Final output sent to browser
+DEBUG - 2022-08-31 06:18:05 --> Total execution time: 0.3849
+INFO - 2022-08-31 10:49:18 --> Config Class Initialized
+INFO - 2022-08-31 10:49:18 --> Hooks Class Initialized
+DEBUG - 2022-08-31 10:49:18 --> UTF-8 Support Enabled
+INFO - 2022-08-31 10:49:18 --> Utf8 Class Initialized
+INFO - 2022-08-31 10:49:18 --> URI Class Initialized
+INFO - 2022-08-31 10:49:18 --> Router Class Initialized
+INFO - 2022-08-31 10:49:18 --> Output Class Initialized
+INFO - 2022-08-31 10:49:18 --> Security Class Initialized
+DEBUG - 2022-08-31 10:49:18 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 10:49:18 --> Input Class Initialized
+INFO - 2022-08-31 10:49:18 --> Language Class Initialized
+INFO - 2022-08-31 10:49:18 --> Loader Class Initialized
+INFO - 2022-08-31 10:49:18 --> Helper loaded: form_helper
+INFO - 2022-08-31 10:49:18 --> Helper loaded: url_helper
+INFO - 2022-08-31 10:49:18 --> Helper loaded: file_helper
+INFO - 2022-08-31 10:49:18 --> Helper loaded: data_helper
+INFO - 2022-08-31 10:49:18 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 10:49:18 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 10:49:18 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 10:49:18 --> Form Validation Class Initialized
+INFO - 2022-08-31 10:49:18 --> Controller Class Initialized
+INFO - 2022-08-31 10:49:18 --> MY_Model class loaded
+INFO - 2022-08-31 10:49:18 --> Model "Usermodel" initialized
+INFO - 2022-08-31 10:49:18 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/login.php
+INFO - 2022-08-31 10:49:18 --> Final output sent to browser
+DEBUG - 2022-08-31 10:49:18 --> Total execution time: 0.0801
+INFO - 2022-08-31 11:53:28 --> Config Class Initialized
+INFO - 2022-08-31 11:53:28 --> Hooks Class Initialized
+DEBUG - 2022-08-31 11:53:28 --> UTF-8 Support Enabled
+INFO - 2022-08-31 11:53:28 --> Utf8 Class Initialized
+INFO - 2022-08-31 11:53:28 --> URI Class Initialized
+INFO - 2022-08-31 11:53:28 --> Router Class Initialized
+INFO - 2022-08-31 11:53:28 --> Output Class Initialized
+INFO - 2022-08-31 11:53:28 --> Security Class Initialized
+DEBUG - 2022-08-31 11:53:28 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 11:53:28 --> Input Class Initialized
+INFO - 2022-08-31 11:53:28 --> Language Class Initialized
+INFO - 2022-08-31 11:53:28 --> Loader Class Initialized
+INFO - 2022-08-31 11:53:28 --> Helper loaded: form_helper
+INFO - 2022-08-31 11:53:28 --> Helper loaded: url_helper
+INFO - 2022-08-31 11:53:28 --> Helper loaded: file_helper
+INFO - 2022-08-31 11:53:28 --> Helper loaded: data_helper
+INFO - 2022-08-31 11:53:28 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 11:53:28 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 11:53:28 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 11:53:28 --> Form Validation Class Initialized
+INFO - 2022-08-31 11:53:28 --> Controller Class Initialized
+INFO - 2022-08-31 11:53:28 --> MY_Model class loaded
+INFO - 2022-08-31 11:53:28 --> Model "Usermodel" initialized
+INFO - 2022-08-31 11:53:28 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/login.php
+INFO - 2022-08-31 11:53:28 --> Final output sent to browser
+DEBUG - 2022-08-31 11:53:28 --> Total execution time: 0.0811
+INFO - 2022-08-31 11:54:14 --> Config Class Initialized
+INFO - 2022-08-31 11:54:14 --> Hooks Class Initialized
+DEBUG - 2022-08-31 11:54:14 --> UTF-8 Support Enabled
+INFO - 2022-08-31 11:54:14 --> Utf8 Class Initialized
+INFO - 2022-08-31 11:54:14 --> URI Class Initialized
+INFO - 2022-08-31 11:54:14 --> Router Class Initialized
+INFO - 2022-08-31 11:54:14 --> Output Class Initialized
+INFO - 2022-08-31 11:54:14 --> Security Class Initialized
+DEBUG - 2022-08-31 11:54:14 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 11:54:14 --> Input Class Initialized
+INFO - 2022-08-31 11:54:14 --> Language Class Initialized
+INFO - 2022-08-31 11:54:14 --> Loader Class Initialized
+INFO - 2022-08-31 11:54:14 --> Helper loaded: form_helper
+INFO - 2022-08-31 11:54:14 --> Helper loaded: url_helper
+INFO - 2022-08-31 11:54:14 --> Helper loaded: file_helper
+INFO - 2022-08-31 11:54:14 --> Helper loaded: data_helper
+INFO - 2022-08-31 11:54:14 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 11:54:14 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 11:54:14 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 11:54:14 --> Form Validation Class Initialized
+INFO - 2022-08-31 11:54:14 --> Controller Class Initialized
+INFO - 2022-08-31 11:54:14 --> MY_Model class loaded
+INFO - 2022-08-31 11:54:14 --> Model "Usermodel" initialized
+INFO - 2022-08-31 11:54:14 --> Final output sent to browser
+DEBUG - 2022-08-31 11:54:14 --> Total execution time: 0.0158
+INFO - 2022-08-31 11:54:18 --> Config Class Initialized
+INFO - 2022-08-31 11:54:18 --> Hooks Class Initialized
+DEBUG - 2022-08-31 11:54:18 --> UTF-8 Support Enabled
+INFO - 2022-08-31 11:54:18 --> Utf8 Class Initialized
+INFO - 2022-08-31 11:54:18 --> URI Class Initialized
+INFO - 2022-08-31 11:54:18 --> Router Class Initialized
+INFO - 2022-08-31 11:54:18 --> Output Class Initialized
+INFO - 2022-08-31 11:54:18 --> Security Class Initialized
+DEBUG - 2022-08-31 11:54:18 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 11:54:18 --> Input Class Initialized
+INFO - 2022-08-31 11:54:18 --> Language Class Initialized
+INFO - 2022-08-31 11:54:18 --> Loader Class Initialized
+INFO - 2022-08-31 11:54:18 --> Helper loaded: form_helper
+INFO - 2022-08-31 11:54:18 --> Helper loaded: url_helper
+INFO - 2022-08-31 11:54:18 --> Helper loaded: file_helper
+INFO - 2022-08-31 11:54:18 --> Helper loaded: data_helper
+INFO - 2022-08-31 11:54:18 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 11:54:18 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 11:54:18 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 11:54:18 --> Form Validation Class Initialized
+INFO - 2022-08-31 11:54:18 --> Controller Class Initialized
+INFO - 2022-08-31 11:54:18 --> MY_Model class loaded
+INFO - 2022-08-31 11:54:18 --> Model "Usermodel" initialized
+INFO - 2022-08-31 11:54:18 --> Final output sent to browser
+DEBUG - 2022-08-31 11:54:18 --> Total execution time: 0.1506
+INFO - 2022-08-31 11:54:18 --> Config Class Initialized
+INFO - 2022-08-31 11:54:18 --> Hooks Class Initialized
+DEBUG - 2022-08-31 11:54:18 --> UTF-8 Support Enabled
+INFO - 2022-08-31 11:54:18 --> Utf8 Class Initialized
+INFO - 2022-08-31 11:54:18 --> URI Class Initialized
+INFO - 2022-08-31 11:54:18 --> Router Class Initialized
+INFO - 2022-08-31 11:54:18 --> Output Class Initialized
+INFO - 2022-08-31 11:54:18 --> Security Class Initialized
+DEBUG - 2022-08-31 11:54:18 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 11:54:18 --> Input Class Initialized
+INFO - 2022-08-31 11:54:18 --> Language Class Initialized
+INFO - 2022-08-31 11:54:18 --> Loader Class Initialized
+INFO - 2022-08-31 11:54:18 --> Helper loaded: form_helper
+INFO - 2022-08-31 11:54:18 --> Helper loaded: url_helper
+INFO - 2022-08-31 11:54:18 --> Helper loaded: file_helper
+INFO - 2022-08-31 11:54:18 --> Helper loaded: data_helper
+INFO - 2022-08-31 11:54:18 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 11:54:18 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 11:54:18 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 11:54:18 --> Form Validation Class Initialized
+INFO - 2022-08-31 11:54:18 --> Controller Class Initialized
+INFO - 2022-08-31 11:54:18 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/header.php
+INFO - 2022-08-31 11:54:18 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/sidebar.php
+INFO - 2022-08-31 11:54:18 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/breadcrumb.php
+INFO - 2022-08-31 11:54:18 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/dashboard.php
+INFO - 2022-08-31 11:54:18 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/footer.php
+INFO - 2022-08-31 11:54:18 --> Final output sent to browser
+DEBUG - 2022-08-31 11:54:18 --> Total execution time: 0.0081
+INFO - 2022-08-31 11:54:25 --> Config Class Initialized
+INFO - 2022-08-31 11:54:25 --> Hooks Class Initialized
+DEBUG - 2022-08-31 11:54:25 --> UTF-8 Support Enabled
+INFO - 2022-08-31 11:54:25 --> Utf8 Class Initialized
+INFO - 2022-08-31 11:54:25 --> URI Class Initialized
+INFO - 2022-08-31 11:54:25 --> Router Class Initialized
+INFO - 2022-08-31 11:54:25 --> Output Class Initialized
+INFO - 2022-08-31 11:54:25 --> Security Class Initialized
+DEBUG - 2022-08-31 11:54:25 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 11:54:25 --> Input Class Initialized
+INFO - 2022-08-31 11:54:25 --> Language Class Initialized
+INFO - 2022-08-31 11:54:25 --> Loader Class Initialized
+INFO - 2022-08-31 11:54:25 --> Helper loaded: form_helper
+INFO - 2022-08-31 11:54:25 --> Helper loaded: url_helper
+INFO - 2022-08-31 11:54:25 --> Helper loaded: file_helper
+INFO - 2022-08-31 11:54:25 --> Helper loaded: data_helper
+INFO - 2022-08-31 11:54:25 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 11:54:25 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 11:54:25 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 11:54:25 --> Form Validation Class Initialized
+INFO - 2022-08-31 11:54:25 --> Controller Class Initialized
+INFO - 2022-08-31 11:54:25 --> MY_Model class loaded
+INFO - 2022-08-31 11:54:25 --> Model "Customermodel" initialized
+INFO - 2022-08-31 11:54:25 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/header.php
+INFO - 2022-08-31 11:54:25 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/sidebar.php
+INFO - 2022-08-31 11:54:25 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/breadcrumb.php
+INFO - 2022-08-31 11:54:25 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/customer/index.php
+INFO - 2022-08-31 11:54:25 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/footer.php
+INFO - 2022-08-31 11:54:25 --> Final output sent to browser
+DEBUG - 2022-08-31 11:54:25 --> Total execution time: 0.0491
+INFO - 2022-08-31 11:54:57 --> Config Class Initialized
+INFO - 2022-08-31 11:54:57 --> Hooks Class Initialized
+DEBUG - 2022-08-31 11:54:57 --> UTF-8 Support Enabled
+INFO - 2022-08-31 11:54:57 --> Utf8 Class Initialized
+INFO - 2022-08-31 11:54:57 --> URI Class Initialized
+INFO - 2022-08-31 11:54:57 --> Router Class Initialized
+INFO - 2022-08-31 11:54:57 --> Output Class Initialized
+INFO - 2022-08-31 11:54:57 --> Security Class Initialized
+DEBUG - 2022-08-31 11:54:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 11:54:57 --> Input Class Initialized
+INFO - 2022-08-31 11:54:57 --> Language Class Initialized
+INFO - 2022-08-31 11:54:57 --> Loader Class Initialized
+INFO - 2022-08-31 11:54:57 --> Helper loaded: form_helper
+INFO - 2022-08-31 11:54:57 --> Helper loaded: url_helper
+INFO - 2022-08-31 11:54:57 --> Helper loaded: file_helper
+INFO - 2022-08-31 11:54:57 --> Helper loaded: data_helper
+INFO - 2022-08-31 11:54:57 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 11:54:57 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 11:54:57 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 11:54:57 --> Form Validation Class Initialized
+INFO - 2022-08-31 11:54:57 --> Controller Class Initialized
+INFO - 2022-08-31 11:54:57 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/header.php
+INFO - 2022-08-31 11:54:57 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/sidebar.php
+INFO - 2022-08-31 11:54:57 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/breadcrumb.php
+INFO - 2022-08-31 11:54:57 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/dashboard.php
+INFO - 2022-08-31 11:54:57 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/footer.php
+INFO - 2022-08-31 11:54:57 --> Final output sent to browser
+DEBUG - 2022-08-31 11:54:57 --> Total execution time: 0.0085
+INFO - 2022-08-31 11:54:58 --> Config Class Initialized
+INFO - 2022-08-31 11:54:58 --> Hooks Class Initialized
+DEBUG - 2022-08-31 11:54:58 --> UTF-8 Support Enabled
+INFO - 2022-08-31 11:54:58 --> Utf8 Class Initialized
+INFO - 2022-08-31 11:54:58 --> URI Class Initialized
+INFO - 2022-08-31 11:54:58 --> Router Class Initialized
+INFO - 2022-08-31 11:54:58 --> Output Class Initialized
+INFO - 2022-08-31 11:54:58 --> Security Class Initialized
+DEBUG - 2022-08-31 11:54:58 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 11:54:58 --> Input Class Initialized
+INFO - 2022-08-31 11:54:58 --> Language Class Initialized
+INFO - 2022-08-31 11:54:58 --> Loader Class Initialized
+INFO - 2022-08-31 11:54:58 --> Helper loaded: form_helper
+INFO - 2022-08-31 11:54:58 --> Helper loaded: url_helper
+INFO - 2022-08-31 11:54:58 --> Helper loaded: file_helper
+INFO - 2022-08-31 11:54:58 --> Helper loaded: data_helper
+INFO - 2022-08-31 11:54:58 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 11:54:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 11:54:58 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 11:54:58 --> Form Validation Class Initialized
+INFO - 2022-08-31 11:54:58 --> Controller Class Initialized
+INFO - 2022-08-31 11:54:58 --> MY_Model class loaded
+INFO - 2022-08-31 11:54:58 --> Model "Usermodel" initialized
+INFO - 2022-08-31 11:54:58 --> Config Class Initialized
+INFO - 2022-08-31 11:54:58 --> Hooks Class Initialized
+DEBUG - 2022-08-31 11:54:58 --> UTF-8 Support Enabled
+INFO - 2022-08-31 11:54:58 --> Utf8 Class Initialized
+INFO - 2022-08-31 11:54:58 --> URI Class Initialized
+INFO - 2022-08-31 11:54:58 --> Router Class Initialized
+INFO - 2022-08-31 11:54:58 --> Output Class Initialized
+INFO - 2022-08-31 11:54:58 --> Security Class Initialized
+DEBUG - 2022-08-31 11:54:58 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-08-31 11:54:58 --> Input Class Initialized
+INFO - 2022-08-31 11:54:58 --> Language Class Initialized
+INFO - 2022-08-31 11:54:58 --> Loader Class Initialized
+INFO - 2022-08-31 11:54:58 --> Helper loaded: form_helper
+INFO - 2022-08-31 11:54:58 --> Helper loaded: url_helper
+INFO - 2022-08-31 11:54:58 --> Helper loaded: file_helper
+INFO - 2022-08-31 11:54:58 --> Helper loaded: data_helper
+INFO - 2022-08-31 11:54:58 --> Database Driver Class Initialized
+DEBUG - 2022-08-31 11:54:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2022-08-31 11:54:58 --> Session: Class initialized using 'files' driver.
+INFO - 2022-08-31 11:54:58 --> Form Validation Class Initialized
+INFO - 2022-08-31 11:54:58 --> Controller Class Initialized
+INFO - 2022-08-31 11:54:58 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/header.php
+INFO - 2022-08-31 11:54:58 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/sidebar.php
+INFO - 2022-08-31 11:54:58 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/breadcrumb.php
+INFO - 2022-08-31 11:54:58 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/dashboard.php
+INFO - 2022-08-31 11:54:58 --> File loaded: /home/u862132972/domains/abandantiya.com/public_html/crm/application/views/common/footer.php
+INFO - 2022-08-31 11:54:58 --> Final output sent to browser
+DEBUG - 2022-08-31 11:54:58 --> Total execution time: 0.0057
